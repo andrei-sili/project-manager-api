799ec87e371496681e17079ca4cc7c4ba0c69bd4
Andrei <andrei.sili1985@gmail.com>
2025-05-06 13:55:59 +0200

    Configured environment variables support and added .env, requirements.txt, and make commands

--- config/settings.py
+++ config/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59)t)g#92a11^zd*&_n)f3lmf(5lzn5o(t3g8+*10c5dcb7mir'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# ENVIRONMENT
+ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ENVIRONMENT == "development"
 
-ALLOWED_HOSTS = []
+if ENVIRONMENT == "development":
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ["mydomain.com"]
 
 
 # Application definition
