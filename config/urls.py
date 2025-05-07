8706b6c5df1bd9d5a1af81bb3ee770612f3c3361
Andrei <andrei.sili1985@gmail.com>
2025-05-07 15:33:21 +0200

    Created UserSerializer and UserViewSet to return authenticated user profile with URLs configured

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-
+auth_urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + auth_urlpatterns
