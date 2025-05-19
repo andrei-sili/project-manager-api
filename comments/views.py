a2baef5f31a32b887e213b2eaff565b04c42265e
Andrei <andrei.sili1985@gmail.com>
2025-05-19 13:24:09 +0200

    Add pagination for top-level task comments with permission checks

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 from comments.models import Comment
 from comments.permisions import IsProjectTeamMember
@@ -11,13 +12,16 @@ from tasks.models import Task
 class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsProjectTeamMember]
 
-    def get_queryset(self):
+    def get_task(self):
         task = get_object_or_404(Task, pk=self.kwargs['task_pk'])
-
         if not task.project.team.members.filter(id=self.request.user.id).exists():
             raise PermissionDenied("You are not a member of this team.")
+        return task
 
-        return Comment.objects.filter(task=task, parent__isnull=True).prefetch_related('replies').order_by('-created_at')
+    def get_queryset(self):
+        task = self.get_task()
+        return Comment.objects.filter(task=task, parent__isnull=True).prefetch_related('replies').order_by(
+            '-created_at')
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update']:
@@ -25,10 +29,16 @@ class CommentViewSet(viewsets.ModelViewSet):
         return CommentSerializer
 
     def perform_create(self, serializer):
-        task_id = self.kwargs['task_pk']
-        task = get_object_or_404(Task, id=task_id)
-
-        if not task.project.team.members.filter(id=self.request.user.id).exists():
-            raise PermissionDenied("You are not a member of this team.")
-
+        task = self.get_task()
         serializer.save(task=task, user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
