ec5c3b5d05fb1797871d415241264c6e6e9b6406
Andrei <andrei.sili1985@gmail.com>
2025-05-21 12:29:11 +0200

    enforce read-only fields validation on profile update

--- apps/users/views.py
+++ apps/users/views.py
@@ -28,13 +28,13 @@ class UserViewSet(viewsets.ViewSet):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['patch'], url_path='update-profile', permission_classes=[permissions.IsAuthenticated])
+    @action(detail=False, methods=['patch'], url_path='update-profile',
+            permission_classes=[permissions.IsAuthenticated])
     def update_profile(self, request):
         serializer = UserSerializer(request.user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'], url_path='change-password',
             permission_classes=[permissions.IsAuthenticated])
