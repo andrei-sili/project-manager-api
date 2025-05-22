87be9a9a985f7ac88109324f8abaa67c4a17f5d8
Andrei <andrei.sili1985@gmail.com>
2025-05-22 12:02:10 +0200

    Add complete test coverage for notification system (DB, WebSocket, API, errors)

--- apps/notify/views.py
+++ apps/notify/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -12,9 +13,9 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user).order_by('-created_at')[:20]
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"], url_path="mark_as_read")
     def mark_as_read(self, request, pk=None):
-        notification = self.get_object()
-        notification.is_read = True
-        notification.save()
-        return Response({'status': 'read'})
+        note = get_object_or_404(Notification, id=pk, user=request.user)
+        note.is_read = True
+        note.save()
+        return Response({"status": "marked as read"})
