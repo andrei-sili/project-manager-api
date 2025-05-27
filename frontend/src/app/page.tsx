331733983e474ca995ef242b16fb1503bcf2c527
Andrei <andrei.sili1985@gmail.com>
2025-05-27 20:38:46 +0200

    delete frontend files

--- frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Bine ai venit în aplicație!</h1>
-      <p>Acesta este conținutul principal.</p>
-    </div>
-  );
-}
