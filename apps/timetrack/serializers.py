b3ad938e1770aac63440b19917dde78ff8adf49b
Andrei <andrei.sili1985@gmail.com>
2025-06-07 01:02:50 +0200

    Complete TimeEntry model and API with summary endpoint and filtering

--- apps/timetrack/serializers.py
+++ apps/timetrack/serializers.py
@@ -2,12 +2,14 @@
 
 from rest_framework import serializers
 from .models import TimeEntry
+from ..tasks.models import Task
 
 
 class TimeEntrySerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-    task = serializers.PrimaryKeyRelatedField(read_only=True)
+    task = serializers.PrimaryKeyRelatedField(queryset=Task.objects.all())
 
     class Meta:
         model = TimeEntry
         fields = ['id', 'user', 'task', 'date', 'minutes', 'note', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
