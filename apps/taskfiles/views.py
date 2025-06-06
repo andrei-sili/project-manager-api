14ebf8129f1d95053cfa1247fbc4534e2d7dd826
Andrei <andrei.sili1985@gmail.com>
2025-06-06 10:49:48 +0200

    Add backend download endpoint for task files, force browser download from frontend

--- apps/taskfiles/views.py
+++ apps/taskfiles/views.py
@@ -1,3 +1,6 @@
+import os
+
+from django.http import Http404, FileResponse
 from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
@@ -6,6 +9,23 @@ from apps.comments.permisions import IsProjectTeamMember
 from apps.taskfiles.models import TaskFile
 from apps.taskfiles.serializers import TaskFileSerializer
 from apps.tasks.models import Task
+
+
+def download_task_file(request, project_id, task_id, file_id):
+    file_obj = get_object_or_404(
+        TaskFile,
+        id=file_id,
+        task__id=task_id,
+        task__project__id=project_id
+    )
+
+    file_path = file_obj.file.path
+    file_name = os.path.basename(file_path)
+
+    if not os.path.isfile(file_path):
+        raise Http404("File not found on disk")
+
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=file_name)
 
 
 class TaskFileViewSet(viewsets.ModelViewSet):
