9ed2ef645e6ff1c288003a7283fce45d164841bf
Andrei <andrei.sili1985@gmail.com>
2025-06-06 10:31:55 +0200

    Switch settings.py to pathlib.Path for BASE_DIR, MEDIA_ROOT and STATIC_ROOT, fix database config for Django 5

--- config/settings.py
+++ config/settings.py
@@ -16,10 +16,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
